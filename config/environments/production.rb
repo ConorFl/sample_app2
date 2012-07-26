2e6879821ff9bda6b9671c702c21ba29cf8bed3f
Con Flanagan <conoreflanagan@gmail.com>
2012-07-25 20:28:32 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ SampleApp2::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
